60b06227f9ef7f3364f1ff035b3e7227500d9403
Olivier Courtin <olivier.courtin@oslandia.com>
2011-05-20 14:27:52 +0000

    default SRS for WFS 1.1.0, must be long
    git-svn-id: file:///tmp/tinyows-svn-mirror/trunk@549 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- src/wfs/wfs_request.c
+++ src/wfs/wfs_request.c
@@ -425,8 +425,10 @@ static void wfs_request_check_srs(ows * o, wfs_request * wr, list * layer_name)
              return;
         }
 
-        if (wr->srs->is_degree && ows_version_get(o->request->version) == 110)
+        if (wr->srs->is_degree && ows_version_get(o->request->version) == 110) {
             wr->srs->is_reverse_axis = true;
+            wr->srs->is_long = true;
+        }
 
     } else {
         b = array_get(o->cgi, "srsname");

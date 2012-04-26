9af6298c8c6b336d3ef7f834c1374ba397d812f4
unknown <MORF07@MB0380.(none)>
2012-04-26 08:07:55 -0400

    Changes wfs_get_feature.c to support mapfile gml_exclude_items.

--- src/wfs/wfs_get_feature.c
+++ src/wfs/wfs_get_feature.c
@@ -115,6 +115,9 @@ void wfs_gml_display_feature(ows * o, wfs_request * wr, buffer * layer_name, buf
 
     /* No Pkey display in GML (default behaviour) */
     if (pkey && pkey->buf && !strcmp(prop_name, pkey->buf) && !o->expose_pk) return;
+	/* Avoid to expose elements in mapfile gml_exclude_items */
+	if (in_list(ows_layer_get(o->layers, layer_name)->exclude_items, prop_name)){ continue; }
+	
 
     if (strlen(value) == 0) return; /* Don't display empty property */ 
 

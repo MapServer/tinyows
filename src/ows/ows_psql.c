bf87237224a7444fa09a9612fe4c5cb07d3f08fa
Frederic Morin <fred.morin@gmail.com>
2012-04-30 10:21:27 -0400

    test

--- src/ows/ows_psql.c
+++ src/ows/ows_psql.c
@@ -303,6 +303,8 @@ buffer *ows_psql_column_constraint_name(ows * o, buffer * column_name, buffer * 
 	buffer_add_str(sql, column_name->buf);
 	buffer_add_str(sql, "'");
 	
+	fprintf(o->output, "constraint name query= '%s'", sql->buf);
+	
 	res = ows_psql_exec(o, sql->buf);
 	
     if (PQresultStatus(res) != PGRES_TUPLES_OK || PQntuples(res) != 1) {

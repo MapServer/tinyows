81f09c386774d0541f5e5294f0e5d2f7316e4c6f
Olivier Courtin <olivier.courtin@oslandia.com>
2012-05-20 13:24:34 +0200

    Axis order it related to GML Version prior to WFS version

--- src/wfs/wfs_request.c
+++ src/wfs/wfs_request.c
@@ -425,7 +425,12 @@ static void wfs_request_check_srs(ows * o, wfs_request * wr, list * layer_name)
              return;
         }
 
-        if (wr->srs->is_degree && ows_version_get(o->request->version) == 110) {
+        if ((wr->format == WFS_GML311 || wr->format == WFS_GML321) && wr->srs->is_degree) {
+            wr->srs->is_reverse_axis = true;
+            wr->srs->is_long = true;
+        } else if (wr->format == WFS_GML311 || wr->format == WFS_GML321) {
+            wr->srs->is_long = true;
+        } else if (ows_version_get(o->request->version) == 110 && wr->srs->is_degree) {
             wr->srs->is_reverse_axis = true;
             wr->srs->is_long = true;
         } else if (ows_version_get(o->request->version) == 110) {
